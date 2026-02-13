741de98a0bb289cbc3cbe251ce738f7f65937ea7
ruri <ruriyoshinova@proton.me>
2026-02-14 05:50:33 +0800

    add hostnames to images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import { withContentlayer } from 'next-contentlayer2';
 
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = { turbopack: {} };
+const nextConfig: NextConfig = {
+    turbopack: {},
+    images: {
+        remotePatterns: [
+            new URL('https://is1-ssl.mzstatic.com/**'),
+            new URL('https://f4.bcbits.com/**')
+        ],
+    },
+};
 
 export default withContentlayer(nextConfig);

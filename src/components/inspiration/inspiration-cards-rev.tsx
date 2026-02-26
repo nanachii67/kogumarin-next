519283b2f243fda4cb01338b1893725fba535fa4
kogskogskogs <rinkoguma313@gmail.com>
2026-02-26 13:39:55 +0800

    fix: raised resolutions for static images from apple music

--- src/components/inspiration/inspiration-cards-rev.tsx
+++ src/components/inspiration/inspiration-cards-rev.tsx
@@ -1,16 +1,18 @@
 'use client';
 
-import { InspirationData } from '@/utils/inspiration-cards';
 import { InspirationCardSeperate } from './inspiration-cards-separate';
+import { InspirationData } from '@/utils/inspiration-cards';
 
 export function InspirationCardsRev() {
     return (
         <div className="relative flex flex-col mx-auto">
-            <div className="relative grid grid-cols-1 md:grid-cols-3 gap-6 max-w-7xl my-5 md:px-10 lg:px-0">
+            <div className="relative grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-7xl my-5">
                 {InspirationData.map((item, index) => (
-                    <InspirationCardSeperate key={item.id}
-                    item={item}
-                    index={index} />
+                    <InspirationCardSeperate
+                        key={item.id}
+                        item={item}
+                        index={index}
+                    />
                 ))}
             </div>
         </div>
--- src/components/release/release-cards-rev.tsx
+++ src/components/release/release-cards-rev.tsx
@@ -6,7 +6,7 @@ import { ReleaseData } from '@/utils/release-cards';
 export function ReleaseCardsRev() {
     return (
         <div className="relative flex flex-col mx-auto">
-            <div className="relative grid grid-cols-1 md:grid-cols-3 gap-6 mt-6 mb-12">
+            <div className="relative grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6 mb-12">
                 {ReleaseData.slice()
                     .reverse()
                     .map((item, index) => (

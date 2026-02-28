2dc03b6cb074278cee1a47d4dbcd5f75659027cd
kogskogskogs <rinkoguma313@gmail.com>
2026-02-28 23:18:51 +0800

    fix: re-added release cover for old design codebase

--- src/utils/new-release.ts
+++ src/utils/new-release.ts
@@ -3,6 +3,7 @@ export const NewRelease = [
         title: `you never left my head`,
         subtitle: '(Acoustic)',
         releaselink: '/releases/you-never-left-my-head-acoustic',
+        releasecover: `https://f4.bcbits.com/img/a3156129443_10.jpg`,
         releasedate: '2026',
     },
 ];

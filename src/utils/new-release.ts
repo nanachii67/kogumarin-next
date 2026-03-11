acd472dc9ce5a7b1800ec54eb523d2a1243f5416
kogskogskogs <rinkoguma313@gmail.com>
2026-03-11 11:43:26 +0800

    fix: added subtitle for new release 3_9_2026

--- src/utils/new-release.ts
+++ src/utils/new-release.ts
@@ -1,6 +1,7 @@
 export const NewRelease = [
     {
         title: `Extraordinary`,
+        subtitle: `(feat. Koguma Rin)`,
         releaselink: '/releases/extraordinary',
         releasecover: `https://f4.bcbits.com/img/a2045670629_10.jpg`,
         releasedate: '2026',
